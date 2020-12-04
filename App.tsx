48fcc0720dce5f40b2d8dda8290609416e49cbcc
米倉謙 <p.p.high.llows@gmail.com>
2020-12-04 23:01:37 +0900

    firstcommit

--- App.tsx
+++ App.tsx
@@ -3,6 +3,22 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  
+  // 任意点
+  const arbitraryPoint = ();
+  // 張力
+  const tension = ();
+  // 電線重量
+  const lineWehight = ();
+  // 重力
+  const g = '9.806554';
+  // 径間長
+  const Spare = ();
+
+  // 任意点弛度の計算
+  const DipResult = lineWehight*g*(Spare-arbitraryPoint)*arbitraryPoint / 2*tension;
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
