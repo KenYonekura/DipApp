42c4b4a23610590aa4960793a3c3e10a666c32f9
米倉謙 <p.p.high.llows@gmail.com>
2020-12-05 22:05:55 +0900

    画面遷移作成

--- src/TableView.tsx
+++ src/TableView.tsx
@@ -2,25 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  
-  // 任意点
-  const arbitraryPoint = ();
-  // 張力
-  const tension = ();
-  // 電線重量
-  const lineWehight = ();
-  // 重力
-  const g = '9.806554';
-  // 径間長
-  const Spare = ();
-
-  // 任意点弛度の計算
-  const Result = lineWehight*g*(Spare-arbitraryPoint)*arbitraryPoint / 2*tension;
-
-  const DispResult = 
-
-
+export default function TableView() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
