e9bea193aa836070ee63f8012da4c7aef942fb9f
米倉謙 <p.p.high.llows@gmail.com>
2020-12-06 08:36:27 +0900

    App.tsxのライブラリ追加

--- App.tsx
+++ App.tsx
@@ -11,40 +11,28 @@ import { Provider as PaperProvider } from "react-native-paper"; // 追加
 
 // Screens
 import { Main } from "./src/Main";
-import { Compose } from "./src/Compose";
+import { Calc } from "./src/Calc";
+import { TableView } from "./src/TableView";
 
 export default function App() {
-
-  // 任意点
-  const arbitraryPoint = ();
-  // 張力
-  const tension = ();
-  // 電線重量
-  const lineWehight = ();
-  // 重力
-  const g = '9.806554';
-  // 径間長
-  const Spare = ();
-
-  // 任意点弛度の計算
-  const Result = lineWehight*g*(Spare-arbitraryPoint)*arbitraryPoint / 2*tension;
-
-  const DispResult = 
-
-
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Clac" component={Calc} />
+          <Stack.Screen name="Main" component={Main} />
+          <Stack.Screen name="TableView" component={TableView} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
