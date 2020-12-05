429638a6e02c27154615e5a88e36c40c79ea18de
米倉謙 <p.p.high.llows@gmail.com>
2020-12-05 22:29:41 +0900

    Clac画面の変数定義

--- App.tsx
+++ App.tsx
@@ -1,9 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+// react navigation ライブラリ
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import "react-native-gesture-handler";
+
+// react native paper ライブラリ
+import { Provider as PaperProvider } from "react-native-paper"; // 追加
+
+// Screens
+import { Main } from "./src/Main";
+import { Compose } from "./src/Compose";
 
 export default function App() {
-  
+
   // 任意点
   const arbitraryPoint = ();
   // 張力
--- src/Clac.tsx
+++ src/Clac.tsx
@@ -3,6 +3,27 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function Calc() {
+  // 任意点
+  const arbitraryPoint = ();
+  // 張力
+  const tension = ();
+  // 電線重量
+  const lineWehight = ();
+  // 重力
+  const g = '9.806554';
+  // 径間長
+  const Spare = ();
+  // 樹高
+  const treeHeight = ();
+
+  const towerHeight = ();
+
+  // 任意点弛度の計算
+  const Result = lineWehight*g*(Spare-arbitraryPoint)*arbitraryPoint / 2*tension;
+
+  // 離隔計算結果
+  const DispResult = towerHeight - treeHeight - Result;
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
