45ca8db740252d649fea85a9a5d5faa4e2d06014
米倉謙 <p.p.high.llows@gmail.com>
2020-12-12 11:54:17 +0900

    FireBaseから電線諸元の値取得完成&Pickerのインポート

--- src/Calc.tsx
+++ src/Calc.tsx
@@ -1,6 +1,6 @@
 import { useNavigation } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Button,
   StyleSheet,
@@ -8,7 +8,19 @@ import {
   View,
   TextInput,
   Pressable,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from "react-native";
+// FireBseに接続するコンポーネント
+import * as firebase from "firebase";
+import "firebase/firestore";
+import RNPickerSelect from "react-native-picker-select";
+
+// FireBaseのから取り出してくる電線諸元の型を定義･･･①
+type Data = {
+  maxT: number;
+  weight: number;
+};
 
 export function Calc() {
   // 画面遷移の定義
@@ -18,12 +30,46 @@ export function Calc() {
     navigation.navigate("Main");
   };
 
+  // FireBaseから取得した電線諸元のデータを格納するフック･･･②
+  // lineDataにはFireBaseから取得したSBACSR160のフィールドの値全てが格納される
+  const [lineData, setlineDatas] = useState<Data[]>([]);
+  console.log(lineData);
+
+  useEffect(() => {
+    getFirebaseItems();
+  }, []);
+  // FireBaseからデータを取得するお作法｡配列で確認する方法
+  // const getFirebaseItems = async () => {
+  //   const lineDataView = await firebase
+  //     .firestore()
+  //     .collection("Powerline")
+  //     .get();
+  //   const PowerlineData = lineDataView.docs.map((doc) => doc.data() as Data);
+  //   setlineDatas(PowerlineData);
+  // };
+  const getFirebaseItems = async () => {
+    const lineDataView = await firebase
+      .firestore()
+      .collection("Powerline")
+      .get();
+    const PowerlineData = lineDataView.docs.map((doc) => doc.data() as Data);
+    setlineDatas(PowerlineData);
+    // console.log(PowerlineData);
+    // console.log(lineDataView);
+  };
+
   // 任意点
   const [ArbitraryPoint, setArbitraryPoint] = React.useState("");
   // 張力
   const [Tension, setTension] = React.useState("");
-  // 電線重量
-  const lineWehight = 0.7663;
+  // 電線重量の表示･･･③
+  // const lineWeight = lineData{SBACSRUGS160.weght};
+  // console.log(lineData);
+  const lineWeight = lineData.map((SBACSRUGS160, index) => (
+    <View style={{ margin: 10 }} key={index.toString()}>
+      <Text>{SBACSRUGS160.weight}</Text>
+    </View>
+  ));
   // 重力
   const g = 9.80665;
   // 径間長
@@ -40,7 +86,7 @@ export function Calc() {
   // 任意点弛度の計算
   const resultDipAdd = () => {
     const result =
-      (lineWehight * g * (Spare - ArbitraryPoint) * ArbitraryPoint) /
+      (lineWeight * g * (Spare - ArbitraryPoint) * ArbitraryPoint) /
       (2 * Tension);
     setResult(result);
   };
@@ -53,35 +99,42 @@ export function Calc() {
 
   return (
     <View style={styles.container}>
+      <Text>電線重量</Text>
+      <Text>{lineWeight}</Text>
       <Text>任意点：</Text>
       <TextInput
         style={{ height: 30, borderColor: "gray", borderWidth: 1 }}
         onChangeText={(text) => setArbitraryPoint(text)}
         value={ArbitraryPoint}
+        keyboardType="numeric"
       />
       <Text>水平張力：</Text>
       <TextInput
         style={{ height: 30, borderColor: "gray", borderWidth: 1 }}
         onChangeText={(text) => setTension(text)}
         value={Tension}
+        keyboardType="numeric"
       />
       <Text>径間長：</Text>
       <TextInput
         style={{ height: 30, borderColor: "gray", borderWidth: 1 }}
         onChangeText={(text) => setSqare(text)}
         value={Spare}
+        keyboardType="numeric"
       />
       <Text>樹高：</Text>
       <TextInput
         style={{ height: 30, borderColor: "gray", borderWidth: 1 }}
         onChangeText={(text) => setTreeHeight(text)}
         value={TreeHeight}
+        keyboardType="numeric"
       />
       <Text>鉄塔高：</Text>
       <TextInput
         style={{ height: 30, borderColor: "gray", borderWidth: 1 }}
         onChangeText={(text) => setTowerHeight(text)}
         value={TowerHeight}
+        keyboardType="numeric"
       />
       <Text>任意点弛度</Text>
       <Button onPress={resultDipAdd} title="計算" />
--- src/TableView.tsx
+++ src/TableView.tsx
@@ -1,34 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { NavigationContext, useNavigation } from '@react-navigation/native';
-import React, { useEffect, useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import { FlatList } from 'react-native-gesture-handler';
-import { loadAll } from "./Store"
+import { StatusBar } from "expo-status-bar";
+import { NavigationContext, useNavigation } from "@react-navigation/native";
+import React, { useEffect, useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+import { loadAll } from "./Store";
 
 export function TableView() {
-  
-  const [datas, setDatas] = useState<Data[]>([]);
+  // 出力データの定義"Data"はData.tsで定義した型
+  // const [datas, setDatas] = useState<Data[]>([]);
 
-  useEffect(() => {
-    const initialize = async () => {
-      const newData = await loadAll();
-      setDatas(newData);
-    };
-    navigation.addListener("focus",initialize);
-  })
+  const datas = [
+    { str: "これだ", key: "これだ" },
+    { str: "bbb", key: "bbbb" },
+    { str: "ccc", key: "cccc" },
+    { str: "ddd", key: "dddd" },
+  ];
+
+  // useEffect(() => {
+  //   const initialize = async () => {
+  //     const newData = await loadAll();
+  //     setDatas(newData);
+  //   };
+  //   navigation.addListener("focus",initialize);
+  // })
 
   // 画面遷移の定義
   const navigation = useNavigation();
   // Calc画面に移動する関数を定義
   const toCalc = () => {
     navigation.navigate("Calc");
-  }
+  };
 
   return (
     <View styles={styles.container}>
       <FlatList
         data={datas}
-        renderItem={({ items }) => <Text>{items.text}</Text>}
+        renderItem={({ item }) => <Text>{item.key}</Text>}
         keyExtractor={(item, index) => index.toString()}
       />
       <Button onPress={toCalc} title="計算に戻る" />
@@ -39,8 +46,8 @@ export function TableView() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
